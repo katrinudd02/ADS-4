28056544c5ee3f416bb4cda9e02d6df0f548ea74
katrinudd02 <97793336+katrinudd02@users.noreply.github.com>
2022-03-20 04:54:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -22,7 +22,7 @@ void SortArr(int arr[], int len) {
 int countPairs1(int* arr, int len, int value) {
     int count = 0;
     bool flag = false;
-    SortArr(int arr[], int len);
+    SortArr(arr[], len);
     for (int i = 0; i < len-1; i++) {
         for (int j = 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {

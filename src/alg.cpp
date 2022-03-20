b3608f3b60695ad527256210284fc579dd35eda2
katrinudd02 <97793336+katrinudd02@users.noreply.github.com>
2022-03-20 21:25:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -41,7 +41,7 @@ int countPairs1(int* arr, int len, int value) {
 int countPairs2(int* arr, int len, int value) {
     int count = 0;
     //SortArr(arr[], len);
-    for (int i = 0; i < len - 1; i++) {
+    for (int i = len-1; i >=0; --i) {
         for (int j = len - 1; j > i; j--) {
             if (arr[i] + arr[j] == value) {
                 count += 1;
@@ -51,23 +51,29 @@ int countPairs2(int* arr, int len, int value) {
     return count;
 }
 
-int cbinsearch(int* arr, int size, int value) {
+int cbinsearch(int* arr, int left1, int size, int value) {
     int count = 0;
-    int left = 0;
-    int right = size - 1;
-    while (left < right) {
-        if (arr[left] == value) {
+    int left2 = left1;
+    int middle=0;
+    int right = size;
+    while (left2 < right-1) {
+        if (arr[middle] == value) {
             count++;
-        }
-        if (arr[right] == value) {
-            count++;
-        }
-        left++;
-        right--;
-    }
-    if (left == right) {
-        if (arr[left] == value) {
-            count++;
+            int el=middle+1;
+            while((arr[el]==value)&&(el<right)){
+                count+=1;
+                el+=1;
+            }
+            el=middle-1;
+            while((arr[el]==value)&&(el>left2)){
+                count+=1;
+                el-=1;
+            }
+            break;
+        } else if (arr[middle] > value) {
+            right=middle;
+        } else {
+            left2=middle;
         }
     }
     return count;
@@ -75,11 +81,11 @@ int cbinsearch(int* arr, int size, int value) {
 
 int countPairs3(int* arr, int len, int value) {
     int count = 0;
-    //SortArr(int arr[], int len);
-    for (int i = 0; i < len; i++) {
-        if (arr[i] <= value - arr[i]) {
-            count += cbinsearch(arr, len, value);
-        }
+    SortArr(int arr[], int len);
+    int el;
+    for (int i = 0; i < len-1; ++i) {
+        el=value-arr[i];
+        count+=cbinsearch(arr, left1, size, value);
     }
     return count;
 }
